533267738eaab1ea6d700575ebbf076801e168b3
assareh <assareh@hashicorp.com>
2021-02-16 20:20:57 -0800

    fixed typo in output

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "azure-appsvc-address" {
     Please note the first time you try to view the web app, it may take
     a couple minutes for the container to boot.
 
-    "azurerm_app_service.app-service.default_site_hostname"
+    "${azurerm_app_service.app-service.default_site_hostname}"
 APPSVC
 }
